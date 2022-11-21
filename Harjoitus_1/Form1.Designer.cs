2274311fb91a8f2dbf0ae0cd20c4a7ec05fbdfaa
SaaraM1 <monkkonen.saara@gmail.com>
2022-11-21 18:39:29 +0200

    Add project files.

---
+++ Harjoitus_1/Form1.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace Harjoitus_1
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.VaihdaBT = new System.Windows.Forms.Button();
+            this.OtsikkoLB = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // VaihdaBT
+            // 
+            this.VaihdaBT.Location = new System.Drawing.Point(154, 163);
+            this.VaihdaBT.Name = "VaihdaBT";
+            this.VaihdaBT.Size = new System.Drawing.Size(135, 66);
+            this.VaihdaBT.TabIndex = 0;
+            this.VaihdaBT.Text = "Vaihda teksti";
+            this.VaihdaBT.UseVisualStyleBackColor = true;
+            this.VaihdaBT.Click += new System.EventHandler(this.VaihdaBT_Click);
+            // 
+            // OtsikkoLB
+            // 
+            this.OtsikkoLB.AutoSize = true;
+            this.OtsikkoLB.Font = new System.Drawing.Font("Showcard Gothic", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.OtsikkoLB.Location = new System.Drawing.Point(73, 42);
+            this.OtsikkoLB.Name = "OtsikkoLB";
+            this.OtsikkoLB.Size = new System.Drawing.Size(352, 89);
+            this.OtsikkoLB.TabIndex = 1;
+            this.OtsikkoLB.Text = "Otsikko";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(829, 450);
+            this.Controls.Add(this.OtsikkoLB);
+            this.Controls.Add(this.VaihdaBT);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Button VaihdaBT;
+        private Label OtsikkoLB;
+    }
+}
