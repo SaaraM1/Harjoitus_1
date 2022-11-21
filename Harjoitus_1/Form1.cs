f40ea9fb3f54ccd01cd0e18921bf3536f21ff3b3
SaaraM1 <monkkonen.saara@gmail.com>
2022-11-21 18:49:02 +0200

    OtsikkoLB.Text muutettu

--- Harjoitus_1/Form1.cs
+++ Harjoitus_1/Form1.cs
@@ -9,7 +9,7 @@ namespace Harjoitus_1
 
         private void VaihdaBT_Click(object sender, EventArgs e)
         {
-            OtsikkoLB.Text = "Heippa Saara!";
+            OtsikkoLB.Text = "Heippa Harri!";
         }
     }
 }
